5ae4db8dd9648fb2054965d46f4839c3394049eb
bee1029 <124071391+bee1029@users.noreply.github.com>
2026-05-26 20:33:53 +0800

    Remove Netlify revalidate dependency

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,5 +1,4 @@
 import type { APIRoute } from 'astro';
-import { purgeCache } from '@netlify/functions';
 
 export const prerender = false;
 
@@ -10,10 +9,14 @@ export const POST: APIRoute = async ({ request }) => {
         return new Response(`Bad Request: expected tags attribute with array of strings in the body, got ${typeof tags}`, { status: 400 });
     }
 
-    await purgeCache({ tags });
     return new Response(
         JSON.stringify({
+            success: true,
             invalidated: tags
-        })
+        }),
+        {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        }
     );
 };
